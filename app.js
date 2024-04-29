110afe8516cfefda72d1102efc3ac74b6af413e7
Vinroy Miltan Dsouza <vinroy2308@gmail.com>
2024-04-29 10:55:45 +0100

    Resolved merge conflicts

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use('/', authInfo, indexRouter);
 app.use('/', authRouters);
 app.use('/', isAuthenticated, usersRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
