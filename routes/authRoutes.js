3f9c07d563fad91dffeb4403218770769eb17be1
Ayush Prajapati <92866992+ayush-p2000@users.noreply.github.com>
2024-04-30 09:14:02 +0100

    Added logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 const {getLoginPage, getRegisterPage, registerUser, verifyEmail, getForgotPassword, getForgotUser, getResetPasswordPage,
-    resetPassword
+    resetPassword, logoutUser
 } = require("../controllers/auth/authController");
 const {passportAuthenticate} = require("../auth/passportAuth");
-const {validateVerification} = require("../middlewares/auth");
+const {validateVerification, isAuthenticated} = require("../middlewares/auth");
 
 router.get('/login', getLoginPage)
 router.get('/verify',  verifyEmail)
 router.post('/login', validateVerification, passportAuthenticate)
+router.post('/logout', isAuthenticated, logoutUser);
 
 router.get('/register', getRegisterPage)
 router.post('/register', registerUser)
