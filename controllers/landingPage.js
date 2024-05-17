8861ea330e7d275372e5ddf3b203d941a7e1c086
Ayush Prajapati <92866992+ayush-p2000@users.noreply.github.com>
2024-05-17 03:59:09 +0100

    login_register
    Merge if needed

--- controllers/landingPage.js
+++ controllers/landingPage.js
@@ -5,11 +5,11 @@ const {getLocation} = require("../public/javascripts/location");
 async function getLandingPage(req, res) {
     //Sample usage for location. Everytime you create an instance, it'll read the current fetched location.
     // const location = await getLocation()
-    res.render('index', {title: 'FloraScan', auth: req.isLoggedIn, user:req.user})
+    res.render('index', {title: 'FloraScan', user:req.user})
 }
 
 async function getWelcomePage(req, res) {
-    res.render('welcome', {title: 'FloraScan', auth: req.isLoggedIn, user:req.user})
+    res.render('welcome', {title: 'FloraScan', user:req.user})
 }
 
 module.exports = {
